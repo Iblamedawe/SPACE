6c10aa35604a694bbc89db2d75d107b09419f459
Skiby <karsai.marko@gmail.com>
2026-01-07 13:13:29 +0100

    Filtering option added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const select = document.getElementById('craftSel');
 const astroCards = document.getElementById('astroCards');
-function orszagokListazasa() {
+var firstLoad = true;
+function orszagokListazasa(filter) {
+    astroCards.innerHTML = '';
     fetch('http://api.open-notify.org/astros.json')
     .then( response =>{
                 if(!response.ok) console.log('Hiba történt!');
@@ -10,19 +12,23 @@ function orszagokListazasa() {
         var name = data.name;
         data.people.forEach( person => {
             console.log(`${person.name} - ${person.craft}`);
+            if(filter === 'all' || person.craft === filter)
             astroCards.innerHTML += `
             <div class="astroCard">
                 <h2>${person.name}</h2>
                 <p>Űrállomás: ${person.craft}</p>
             </div>`;
         });
-        var stations = data.people.map(person => person.craft);
-        stations = [...new Set(stations)];
-        stations.forEach( station => {
-            craftSel.innerHTML += `<option value="${station}">${station}</option>`;
-        });
-        document.getElementById('population').innerText = data.number;
+        if(firstLoad){
+            firstLoad = false;
+            var stations = data.people.map(person => person.craft);
+            stations = [...new Set(stations)];
+            stations.forEach( station => {
+                craftSel.innerHTML += `<option value="${station}">${station}</option>`;
+            });
+            document.getElementById('population').innerText = data.number;
+        }
     });
 }
-
-orszagokListazasa();
+document.getElementById('craftSel').addEventListener('change', (event) => {orszagokListazasa(event.target.value);});
+orszagokListazasa('all');
