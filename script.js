a7103cf6347f5c6fd0690828f52880b9b0732d19
Skiby <karsai.marko@gmail.com>
2026-01-07 13:00:10 +0100

    Select functioning

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const select = document.getElementById('craftSel');
 function orszagokListazasa() {
     fetch('http://api.open-notify.org/astros.json')
-    .then(response => response.json())
+    .then( response =>{
+                if(!response.ok) console.log('Hiba történt!');
+                return response.json();
+            })
     .then(data => {
-        const name = data.name;
-        const craft = data.craft;
-        const select = document.getElementById('name');
-        const option = document.createElement('option');
-        option.value = craft;
-        option.textContent = craft;
-        select.appendChild(option);
+        var name = data.name;
+        data.people.forEach( person => {
+            console.log(`${person.name} - ${person.craft}`);
+        });
+        var stations = data.people.map(person => person.craft);
+        stations = [...new Set(stations)];
+        stations.forEach( station => {
+            craftSel.innerHTML += `<option value="${station}">${station}</option>`;
+        });
+        document.getElementById('population').innerText = data.number;
     });
 }
+
+orszagokListazasa();
